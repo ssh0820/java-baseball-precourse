80698598f51ccc1398ce529f0418e49f4c395796
seoseunghyeon <tmdgustj@gmail.com>
2022-10-03 18:09:01 +0800

    refactor : 사용자 입력값 단위별 테스트

--- src/test/java/baseball/domain/UserTest.java
+++ src/test/java/baseball/domain/UserTest.java
@@ -10,18 +10,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
 
     @Test
-    @DisplayName("사용자 입력값 유효성 체크")
-    void inputlengthCheck(){
+    @DisplayName("사용자가 3개의 값만 입력했을때")
+    void inputlength3(){
         User user = new User();
-
         assertTrue(user.inputValidation("123"));
+    }
 
+    @Test
+    @DisplayName("사용자가 2개의 값만 입력했을때")
+    void inputlength2(){
+        User user = new User();
         assertThatIllegalArgumentException().isThrownBy(()-> user.inputValidation("12"));
+    }
+
+    @Test
+    @DisplayName("사용자가 4개의 값만 입력했을때")
+    void inputlength4(){
+        User user = new User();
         assertThatIllegalArgumentException().isThrownBy(()-> user.inputValidation("1234"));
-        assertThatIllegalArgumentException().isThrownBy(()-> user.inputValidation("aa"));
+    }
+
+    @Test
+    @DisplayName("사용자가 특수문자를 입력했을때")
+    void inputlength_(){
+        User user = new User();
         assertThatIllegalArgumentException().isThrownBy(()-> user.inputValidation("@@"));
-        assertThatIllegalArgumentException().isThrownBy(()-> user.inputValidation("   "));
-        assertThatIllegalArgumentException().isThrownBy(()-> user.inputValidation("  1 "));
     }
 
 }
